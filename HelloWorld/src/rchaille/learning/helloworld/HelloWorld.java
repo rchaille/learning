68694d3beca1384146e668af1a7d22f3e1ea4589
Rebecca <Rebecca@192.168.1.137>
2016-09-20 22:25:56 -0400

    re-factored into a main and a class....added constructor

---
+++ HelloWorld/src/rchaille/learning/helloworld/HelloWorld.java
@@ -0,0 +1,18 @@
+package rchaille.learning.helloworld;
+
+public class HelloWorld {	
+	
+	private String greeting;
+	
+	//constructor
+	public HelloWorld()
+	{
+		greeting = "Hello, World!";
+	}
+
+	public void printGreeting () {
+			
+		System.out.println(greeting);
+	}
+
+}
---
+++ HelloWorld/src/rchaille/learning/helloworld/HelloWorldMain.java
@@ -0,0 +1,9 @@
+package rchaille.learning.helloworld;
+
+public class HelloWorldMain {	
+
+	public static void main (String [] args) {
+	
+		new HelloWorld().printGreeting();
+	}
+}
